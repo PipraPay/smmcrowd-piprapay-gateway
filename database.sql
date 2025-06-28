f9917e7c71ee6338e6bce21d7a46bd321af32fd2
Mouzh Hossain Shad <shadmouzhhossain@gmail.com>
2025-06-28 14:11:31 +0600

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,7 @@
+INSERT INTO `gateways` 
+(`id`, `form_id`, `code`, `name`, `alias`, `status`, `gateway_parameters`, `supported_currencies`, `crypto`, `extra`, `description`, `created_at`, `updated_at`) 
+VALUES 
+('175', '0', '175', 'PipraPay Payment Gateway', 'PipraPay', '1', 
+'{\"api_key\":{\"title\":\"API KEY\",\"global\":true,\"value\":\"\"},\"api_url\":{\"title\":\"API URL\",\"global\":true,\"value\":\"https:\\/\\/sandbox.piprapay.com\"},\"currency\":{\"title\":\"Currency\",\"global\":true,\"value\":\"BDT\"}}',
+'{\"BDT\":\"BDT\",\"AUD\":\"AUD\",\"BRL\":\"BRL\",\"CAD\":\"CAD\",\"CZK\":\"CZK\",\"DKK\":\"DKK\",\"EUR\":\"EUR\",\"HKD\":\"HKD\",\"HUF\":\"HUF\",\"INR\":\"INR\",\"ILS\":\"ILS\",\"JPY\":\"JPY\",\"MYR\":\"MYR\",\"MXN\":\"MXN\",\"TWD\":\"TWD\",\"NZD\":\"NZD\",\"NOK\":\"NOK\",\"PHP\":\"PHP\",\"PLN\":\"PLN\",\"GBP\":\"GBP\",\"RUB\":\"RUB\",\"SGD\":\"SGD\",\"SEK\":\"SEK\",\"CHF\":\"CHF\",\"THB\":\"THB\",\"USD\":\"$\"}', 
+'0', NULL, NULL, '2019-09-14 19:14:22', '2023-08-28 14:13:15');
